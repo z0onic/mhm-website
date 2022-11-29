494f9d182579e54e3ecd6454c26006cc839adad7
z0onic <jthickson13@gmail.com>
2022-11-29 10:10:06 -0500

    1.16

--- src/Comps/Nav.js
+++ src/Comps/Nav.js
@@ -6,28 +6,30 @@ import Cranes from '../Imgs/background.jpg'
 function Nav() {
     return (
         <div className="header">
-            <img className="header-bkgd" src={Cranes} alt="cranes" />
-            <nav className="menu">
-                <ul>
-                    <li>
-                        <Link className="nav-link" to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-link" to="/services">Services</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                </ul>
-            </nav>
+            {/* <img className="header-bkgd" src={Cranes} alt="cranes" /> */}
+            <input id="menu-toggle" type="checkbox" />
+            <label className="menu-button-container" for="menu-toggle">
+                <div className="menu-button"></div>
+            </label>
+            <ul className="menu">
+                <li>
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li>
+                    <Link className="nav-link" to="/services">Services</Link>
+                </li>
+                <li>
+                    <Link className="nav-link" to="/about">About</Link>
+                </li>
+            </ul>
             <div className="header-info">
                 <div className="logo">
                     <img className="logo-img" src={Logo} alt="mhm logo" />
                 </div>
-                <div className="contact">
+                {/* <div className="contact">
                     <h2 className="cell">cell: 704-930-9692</h2>
                     <h2 className="">email: tom.hickson@mhm.coach</h2>
-                </div>
+                </div> */}
             </div>
         </div>
     )
