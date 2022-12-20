1f48c9ca1b1c8c9fb1b2bfa21e76c9e5e0538fd6
z0onic <59104479+z0onic@users.noreply.github.com>
2022-12-20 13:43:43 -0500

    add js functionality

--- src/Comps/Nav.js
+++ src/Comps/Nav.js
@@ -5,6 +5,15 @@ import {ReactComponent as Phone} from '../Imgs/phone-solid.svg'
 import {ReactComponent as Email} from '../Imgs/envelope-solid.svg'
 
 function Nav() {
+    const menu = document.querySelector('#menu-toggle')
+    const links = document.querySelectorAll('.nav-link')
+    React.useEffect(() => {
+        const phone = document.querySelector('.cell')
+        phone.addEventListener('click', () => window.open('tel:7049309692'))
+    })
+    links.forEach(item => {
+        item.addEventListener('click', () => menu.checked = false)
+    })
     return (
         <div className="header">
             <input id="menu-toggle" type="checkbox" />
@@ -31,7 +40,11 @@ function Nav() {
                 </div>
                 <div className="contact">
                     <h2 className="cell"><Phone />704-930-9692</h2>
-                    <h2 className="email"><Email />tom.hickson@mhm.coach</h2>
+                    <h2 className="email"><Email /><a 
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=email@tom.hickson@mhm.coach" 
+                        target="_blank" 
+                        rel="noreferrer"
+                    >tom.hickson@mhm.coach</a></h2>
                 </div>
             </div>
         </div>
